cb335660ea96e4522d44286944cc17ef076c0373
defy <8841170+defydef@users.noreply.github.com>
2023-10-19 07:38:07 +0700

    Implement Counter using compount component pattern

---
+++ src/Counter.js
@@ -0,0 +1,44 @@
+import { createContext, useContext, useState } from "react";
+
+// 1. Create a context
+const CounterContext = createContext();
+
+// 2. Create a parent component
+function Counter({ children }) {
+  const [count, setCount] = useState(0);
+  const increase = () => setCount((c) => c + 1);
+  const decrease = () => setCount((c) => c - 1);
+
+  return (
+    <CounterContext.Provider value={{ count, increase, decrease }}>
+      <span>{children}</span>
+    </CounterContext.Provider>
+  );
+}
+
+// 3.Create child components that implement common tasks
+function Count() {
+  const { count } = useContext(CounterContext);
+  return <span> {count} </span>;
+}
+
+function Increase({ icon }) {
+  const { increase } = useContext(CounterContext);
+  return <button onClick={increase}>{icon}</button>;
+}
+
+function Decrease({ icon }) {
+  const { decrease } = useContext(CounterContext);
+  return <button onClick={decrease}>{icon}</button>;
+}
+
+function Label({ children }) {
+  return <span> {children} </span>;
+}
+
+// 4. Add child components to the parent component
+Counter.Count = Count;
+Counter.Increase = Increase;
+Counter.Decrease = Decrease;
+Counter.Label = Label;
+export default Counter;
